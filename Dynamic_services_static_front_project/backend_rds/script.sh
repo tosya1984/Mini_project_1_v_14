9285d44719538722d7a4e8656f8a70d41f1697d6
Anton Tsiatska <87123854+tosya1984@users.noreply.github.com>
2024-10-16 01:31:58 +0300

    Create script.sh

---
+++ Dynamic_services_static_front_project/backend_rds/script.sh
@@ -0,0 +1,10 @@
+#$/bin/bash
+sudo docker run -dp 5000:5000 \
+        -e CORS_ALLOWED_ORIGINS="http://13.60.210.138,http://localhost,http://127.0.0.1,http://d26yhfzwzeen3x.cloudfront.net" \
+        -e DB_HOST="firstpostgresql.cjaawu4o67uy.eu-north-1.rds.amazonaws.com" \
+        -e DB_PORT=5432 \
+        -e DB_USER="postgre" \
+        -e DB_PASSWORD="tosya134526" \
+        -e DB_NAME="postgres" \
+        -e DEBUG="False" \
+        myrdsapp
