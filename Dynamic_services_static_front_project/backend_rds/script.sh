0ac99fc7425d15771025dfc841f868a3498d6270
Anton Tsiatska <87123854+tosya1984@users.noreply.github.com>
2024-10-30 00:31:10 +0200

    Update script.sh

--- Dynamic_services_static_front_project/backend_rds/script.sh
+++ Dynamic_services_static_front_project/backend_rds/script.sh
@@ -1,9 +1,9 @@
 #$/bin/bash
 sudo docker run -dp 5000:5000 \
         -e CORS_ALLOWED_ORIGINS="http://13.60.5.228,http://localhost,http://127.0.0.1,http://d26yhfzwzeen3x.cloudfront.net" \
-        -e DB_HOST="myrdsec2.cjaawu4o67uy.eu-north-1.rds.amazonaws.com" \
+        -e DB_HOST="myrds.cjaawu4o67uy.eu-north-1.rds.amazonaws.com" \
         -e DB_PORT=5432 \
-        -e DB_USER="postgre" \
+        -e DB_USER="postgres" \
         -e DB_PASSWORD="tosya134526" \
         -e DB_NAME="postgres" \
         myrdsapp
