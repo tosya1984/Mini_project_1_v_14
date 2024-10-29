1469ad8241bb1bf063f0ebb9c74d85861745b652
Anton Tsiatska <87123854+tosya1984@users.noreply.github.com>
2024-10-30 00:34:25 +0200

    Update script.sh

--- Dynamic_services_static_front_project/backend_redis/script.sh
+++ Dynamic_services_static_front_project/backend_redis/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo docker run -dp 5001:5000 \
-        -e CORS_ALLOWED_ORIGINS="http://13.60.210.138,http://localhost,http://127.0.0.1,http://d26yhfzwzeen3x.cloudfront.net" \
+sudo docker run -dp 5001:5001 \
+        -e CORS_ALLOWED_ORIGINS="http://13.60.5.228,http://localhost,http://127.0.0.1,http://d26yhfzwzeen3x.cloudfront.net" \
         -e REDIS_HOST="clustercfg.secondredis.npd7ue.eun1.cache.amazonaws.com" \
         -e REDIS_PORT="6379" \
         -e REDIS_DB="" \
